2441c15b22b8c727d0fb853bcaea9cc739da23a0
IlyaSolovyev <solovyev.21@yandex.ru>
2022-10-04 21:58:13 +0300

    templates at terminal com port setup modal footer
    added jinja workflow

--- main.py
+++ main.py
@@ -2,4 +2,10 @@ import eel
 
 if __name__ == '__main__':
     eel.init('static_web_folder', allowed_extensions=['.js', '.html'],)
-    eel.start('main.html', mode='chrome', size=(720, 480), position=(0,0))
+    eel.start(
+        'templates/main.html',
+        jinja_templates='templates',
+        mode='chrome',
+        size=(720, 480),
+        position=(0,0)
+    )
