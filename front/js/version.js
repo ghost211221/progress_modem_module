eccecb496f95ec170f4ee54f04d1f127fabc940f
IlyaSolovyev <solovyev.21@yandex.ru>
2023-08-30 21:27:36 +0300

    about

--- front/js/version.js
+++ front/js/version.js
@@ -1,4 +1,5 @@
 $(document).ready(async function() {
     version = await eel.get_version()();
     document.title = `Графическая программа настройки модуля ПН6280 ИЛТА.464512.005 v${version}`
+    $('#program_info_version').text(` ${version}`);
 })
